f7d2543de2699759257049ca9856803ef3d0b314
Mark <menachemsalzberg13@gmail.com>
2025-07-01 16:59:49 +0300

    Refactor student creation logic and add read method.

--- modules/create.js
+++ modules/create.js
@@ -4,19 +4,22 @@ import { readFile, writeFile } from "node:fs";
 function create(student) {
     readFile('C:\\JSProjects\\SchoolManagerCRUD\\DB\\DB.txt', 'utf8', (err, data) => {
         if (err) {
-            throw new Error("Error reading file!")
+            throw new Error("Error reading file!");
         } else {
             let dataArray = JSON.parse(data);
+            let maxId = 0;
+            if (dataArray.length > 0) {
+                maxId = Math.max(...dataArray.map(s => parseInt(s.Id, 10) || 0));
+            }
+            student.Id = String(maxId + 1);
             dataArray.push(student);
             writeFile('C:\\JSProjects\\SchoolManagerCRUD\\DB\\DB.txt', JSON.stringify(dataArray, null, 2), (err) => {
                 if (err) {
-                    throw new Error("Error writing to file!")
-                } else {
-
+                    throw new Error("Error writing to file!");
                 }
-            })
+            });
         }
-    })
+    });
 }
 
-//create({ "Id": "2", "Name": "Adina", "Age": "23", "Grade": "10" });
+create({ "Name": "Menavc", "Age": "24", "Grade": "12" });
--- modules/read.js
+++ modules/read.js
@@ -1,1 +1,17 @@
 import { readFile, writeFile } from "node:fs";
+
+function read(cb) {
+    readFile('C:\\JSProjects\\SchoolManagerCRUD\\DB\\DB.txt', 'utf8', (err, data) => {
+        if (err) {
+            cb(err, null);
+        } else {
+            let dataArray = JSON.parse(data);
+            cb(null, dataArray);
+        }
+    });
+}
+
+// read((err, students) => {
+//     if (err) console.error(err);
+//     else console.log(students);
+// });
