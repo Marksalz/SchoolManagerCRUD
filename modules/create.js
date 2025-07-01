bfda6b18c4fdc11b857b9e34e87c6af2eccdaf1c
Mark <menachemsalzberg13@gmail.com>
2025-07-01 16:04:43 +0300

    Refactor student data in DB and implement create function for adding new students

--- modules/create.js
+++ modules/create.js
@@ -1,1 +1,22 @@
+import { error } from "node:console";
 import { readFile, writeFile } from "node:fs";
+
+function create(student) {
+    readFile('C:\\JSProjects\\SchoolManagerCRUD\\DB\\DB.txt', 'utf8', (err, data) => {
+        if (err) {
+            throw new Error("Error reading file!")
+        } else {
+            let dataArray = JSON.parse(data);
+            dataArray.push(student);
+            writeFile('C:\\JSProjects\\SchoolManagerCRUD\\DB\\DB.txt', JSON.stringify(dataArray, null, 2), (err) => {
+                if (err) {
+                    throw new Error("Error writing to file!")
+                } else {
+
+                }
+            })
+        }
+    })
+}
+
+//create({ "Id": "2", "Name": "Adina", "Age": "23", "Grade": "10" });
